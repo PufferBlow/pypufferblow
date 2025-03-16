c98583b20767eff53ee2db052899edf5c1aa41b0
ramsy0dev <ramsy0dev@gmail.com>
2025-03-15 17:18:28 -0700

    update(conftest.py): Fix issue with preserving the value of ValueStorage object

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,12 @@ class ValueStorage:
     """
     Value storage class for sharing constants across tests cases
     """
-    username                    :   str     =   "user1"
-    password                    :   str     =   "12345678"
-    new_username                :   str     =   "new_user1"
-    new_password                :   str     =   "123456789"
-    auth_token                  :   None
-    bad_formated_auth_token     :   str     =   "abcdwqwpdopok"
+    def __init__(self):
+        self.username : str = "user1"
+        self.password : str = "12345678"
+        self.new_username : str = "new_user1"
+        self.new_password : str = "123456789"
+        self.auth_token : str = None
+        self.bad_formated_auth_token : str = "abcdwqwpdopok"
+
+value_storage = ValueStorage()
